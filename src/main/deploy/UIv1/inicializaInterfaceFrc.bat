111821e019460811aaf15ceb5eb278f7525daf28
Kaique Siqueira <kaiquedepaula666@gmail.com>
2025-10-12 11:43:30 -0300

    update .bat

--- src/main/deploy/UIv1/inicializaInterfaceFrc.bat
+++ src/main/deploy/UIv1/inicializaInterfaceFrc.bat
@@ -1,5 +1,5 @@
 @echo off
-cd "C:\Users\Ricardo\Documents\GitHub\TouchScreenInterface\Example\src\main\deploy\UIv1"
+cd "C:\Users\Ricardo\Documents\GitHub\SwerveDriveKraken\src\main\depoy\UIv1"
 start http://localhost:8888
 pynetworktables2js --robot roborio-7563-frc.local
 
