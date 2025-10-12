0fbc1e4efad1143542f5fcd51a5be620d74496a9
Kaique Siqueira <kaiquedepaula666@gmail.com>
2025-10-12 11:45:01 -0300

    correct .bat

--- src/main/deploy/UIv1/inicializaInterfaceFrc.bat
+++ src/main/deploy/UIv1/inicializaInterfaceFrc.bat
@@ -1,5 +1,5 @@
 @echo off
-cd "C:\Users\Ricardo\Documents\GitHub\SwerveDriveKraken\src\main\depoy\UIv1"
+cd "C:\Users\Instrutor\Documents\GitHub\SwerveDriveKraken\src\main\deploy\UIv1"
 start http://localhost:8888
 pynetworktables2js --robot roborio-7563-frc.local
 
